00eece320bcc9afbdec617591a1cb95b5a84e2d1
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2021-09-27 17:14:56 +0200

    Inclusion des fichiers html header.html.php et footer.html.php dans index.html.php

--- views/article/index.html.php
+++ views/article/index.html.php
@@ -1,14 +1,21 @@
-<!-- Afficher la donnée récupérer de la bdd -->
 <?php
+// Afficher la donnée récupérer de la bdd
 
+require TEMPLATES . DIRECTORY_SEPARATOR . "header.html.php";
 // les instructions qui afficheront les données récupérées dans la bdd
 
 foreach ($articles as $article):
 ?>
+<h1>Liste des articles</h1>
+
 <article>
     <h2><?= $article->getTitle() ?></h2>
     <span><?= $article->getCreatedAt() ?></span>
     <p><?= nl2br($article->getContent()) ?></p>
     <a href="<?= sprintf('/article/%d/show', $article->getId()) ?>">Voir l'article</a>
 </article>
-<?php endforeach; ?>
+<?php
+endforeach;
+
+require TEMPLATES . DIRECTORY_SEPARATOR . "footer.html.php";
+?>
