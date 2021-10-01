3b736365ad0f9cc268dd433aca7c85509d1e2e6b
Thibaut CHIRON <chiron.thibaut@sfr.fr>
2021-10-01 15:33:12 +0200

    Affichage d'un message dans article/index.html.php si aucun article

--- views/article/index.html.php
+++ views/article/index.html.php
@@ -1,9 +1,15 @@
 <h1>Liste des articles</h1>
-<?php foreach ($articles as $article) : ?>
+<?php if (!empty($articles)) :
+foreach ($articles as $article) : ?>
     <article>
         <h2><?= $article->getTitle() ?></h2>
         <span><?= $article->getCreatedAt() ?></span>
         <p><?= nl2br($article->getContent()) ?></p>
         <a href="<?= sprintf('/article/%d/show', $article->getId()) ?>">Voir l'article</a>
     </article>
-<?php endforeach; ?>
+<?php endforeach;
+else : ?>
+<div>
+    <b>Nothing to see here !</b>
+</div>
+<?php endif; ?>
